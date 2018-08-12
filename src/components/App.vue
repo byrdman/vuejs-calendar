3584c4bb0bc8f8c7e921ef48c699f6617c20be3c
Sam Byrd <sbyrd@eeft>
2018-08-12 14:55:25 -0500

    add previous and after days

--- src/components/App.vue
+++ src/components/App.vue
@@ -7,12 +7,13 @@
   export default {
     data() {
       return {
-        month: 2,
+        month: 5,
         year: 2017
       };
     },
     computed: {
       days() {
+        // Generating all days in current month
         let days = [];
         let currentDay = this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D');
         days.push(currentDay);
@@ -20,6 +21,23 @@
           days.push(currentDay);
           currentDay = this.$moment(currentDay).add(1, 'days');
         } while ((currentDay.month()+1) === this.month);
+
+        // add previous days to start
+        const SUNDAY = 0;
+        const MONDAY = 1;
+        currentDay = this.$moment(days[0]);
+        while (currentDay.day() !== MONDAY) {
+          currentDay = this.$moment(currentDay).subtract(1, 'days');
+          days.unshift(currentDay);
+        };
+
+        // add extra days to end
+        currentDay = this.$moment(days[days.length-1]);
+        while (currentDay.day() !== SUNDAY) {
+          currentDay = this.$moment(currentDay).add(1, 'days');
+          days.push(currentDay);
+        }
+
         return days;
       }
     }
